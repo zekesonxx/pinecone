f68f4c214260d80dea751e79c1c28b95956c3857
Zeke Sonxx <zeke@zekesonxx.com>
2014-05-13 15:03:39 -0400

    Inital Commit

---
+++ try.js
@@ -0,0 +1,7 @@
+
+console.log('Hello');
+a = 1;
+var b = 2;
+if (a == b) {
+  console.log('Bacon');
+}
---
+++ try.lua
@@ -0,0 +1,6 @@
+--# Converted using js2lua
+
+console.log("Hello")
+a = 1
+local b = 2
+-- Unable to convert code from 45 to 86 (Unknown action)
