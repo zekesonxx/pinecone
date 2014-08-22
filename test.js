28ec48e1074550271c8e52a0fb4bfad87a89b9ba
Zeke Sonxx <zeke@zekesonxx.com>
2014-08-22 18:26:53 -0400

    Some cleanup, made it a bit more formal

--- test.js
+++ test.js
@@ -1,10 +1,9 @@
+'use strict';
 var pinecone = require('./lib');
-var acorn = require('acorn');
 var fs = require('fs');
 
 var input = fs.readFileSync('./try.js', 'utf8');
-var parsed = acorn.parse(input, {});
-var converted = pinecone(parsed);
+var converted = pinecone.convert(input);
 
 fs.writeFileSync('./try.lua', converted, 'utf8');
 //console.log(converted);
