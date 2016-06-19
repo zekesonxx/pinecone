c451394c5332a3086735345018cd99f19ace5e5a
Zeke Sonxx <zeke@zekesonxx.com>
2016-06-19 02:24:09 -0400

    temp commit sorry will fix later

---
+++ runtime/main.js
@@ -0,0 +1,34 @@
+'use strict';
+/* globals _G, PineconeRuntime, pinecone__comment, pinecone__version, pinecone__rawlua*/
+/**
+ * Pinecone Runtime
+ * Some notes:
+ * * The runtime cannot depend on itself.
+ *   This means more usage of pinecone__rawlua then you would think to avoid compiler runtiming
+ * * Speed (in lua) should be prioritized over speed (in js) or code cleanliness
+ * * Inlining tricks into the compiler isn't abhorrent, and helps keep the runtime lightweight
+ */
+
+if (!_G.PineconeRuntime) {
+  pinecone__comment('Pinecone Runtime, LGPL3 licensed');
+  _G.PineconeRuntime = {};
+  PineconeRuntime.version = pinecone__version();
+
+  PineconeRuntime.concat = function (a, b) {
+    if (typeof a !== 'string' && typeof b !== 'string' ) {
+      return pinecone__rawlua('a+b');
+    } else {
+      return pinecone__rawlua('a..b');
+    }
+  };
+
+  PineconeRuntime.replacements = {
+    'nil': 'undefined',
+    'table': 'object'
+  };
+  PineconeRuntime.typeof = function(variable) {
+    /* globals type */
+    var luaType = type(variable);
+    return PineconeRuntime.replacements[luaType] || luaType;
+  };
+}
---
+++ test.sh
@@ -0,0 +1,2 @@
+acorn try.js > try.json
+"$PWD/bin/pinecone" --ast try.json -o try.lua
