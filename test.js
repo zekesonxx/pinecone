47a146f29ab1f19b70b10e71dfd93138e7e7972a
Zeke Sonxx <zeke@zekesonxx.com>
2016-08-14 15:59:31 -0400

    pinecone2

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
 'use strict';
-var pinecone = require('./lib');
+var compiler = require('./lib');
+var acorn = require('acorn');
 var fs = require('fs');
 
 var input = fs.readFileSync('./try.js', 'utf8');
-var converted = pinecone.convert(input);
+var converted = compiler.compile(acorn.parse(input, {}));
 
 fs.writeFileSync('./try.lua', converted, 'utf8');
-//console.log(converted);
+console.log(converted);
--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 acorn try.js > try.json
-"$PWD/bin/pinecone" --ast try.json -o try.lua
+node "$PWD/test.js"
